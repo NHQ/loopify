e090abcbf1dc8819dda1ab414f82f3a8efc822b1
johnnyscript <mostmodernist@gmail.com>
2016-01-08 19:53:40 -0800

    dank loops

--- entry.js
+++ entry.js
@@ -1,11 +1,16 @@
 var master = new AudioContext
 var loopy = require('./')(master)
 
-loopy.add({
+// add and play methods
+
+loopy.add({ // load from relative path
   'perc1' : 'drum1.wav',
   'perc2' : 'drum2.wav'
-}, function(err){
-  var src1 = loopy.play('perc1', 0)
-  var src2 = loopy.play('perc2', .5)
+}, function(err){ // callback happens after all downloads complete
+
+  // play returns the web audio source node
+  var src1 = loopy.play('perc1')
+  var src2 = loopy.play('perc2')
   src1.playbackRate.value = .5
+
 })
---
+++ readme.md
@@ -0,0 +1,17 @@
+# loopify
+
+A web audio sample loader n looper.  See [entry.js](entry.js) for use.
+
+To test run, clone and do
+
+```
+npm install 
+npm run bundle
+ecstatic /public
+```
+
+to install ecstatic, do
+
+```
+npm install -g ecstatic
+```
